d5289f082f8b749464d3391fb13bc7509861b5a1
rahmatberl <rahmatberlianda@gmail.com>
2021-12-07 15:05:40 +0700

    dashboard test

--- routes/web.php
+++ routes/web.php
@@ -23,5 +23,5 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('test', function () {
-    return view('layouts.master');
+    return view('halo');
 });
